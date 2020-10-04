a00143c5eaa64c42f4f9224b1c64c941a3857b9a
Ghazi Khan <ghazikhan205@gmail.com>
2020-10-04 17:29:15 +0530

    Updates

---
+++ precache-manifest.3b119cf2ae83d2d82a8b759525d7f3da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260379b960fe0104c09dfb88bfdd70fd",
+    "url": "/cwg-responsive-tester/index.html"
+  },
+  {
+    "revision": "abbf6cb720e029a818a7",
+    "url": "/cwg-responsive-tester/static/css/2.319037af.chunk.css"
+  },
+  {
+    "revision": "30f9eab40ec794e6e17d",
+    "url": "/cwg-responsive-tester/static/css/main.3a96fc0e.chunk.css"
+  },
+  {
+    "revision": "abbf6cb720e029a818a7",
+    "url": "/cwg-responsive-tester/static/js/2.9413f08f.chunk.js"
+  },
+  {
+    "revision": "31ac725c350433707f281916a808f78f",
+    "url": "/cwg-responsive-tester/static/js/2.9413f08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f9eab40ec794e6e17d",
+    "url": "/cwg-responsive-tester/static/js/main.1f03e969.chunk.js"
+  },
+  {
+    "revision": "cc703c4d793aea6ee5cd",
+    "url": "/cwg-responsive-tester/static/js/runtime-main.d44db0d0.js"
+  },
+  {
+    "revision": "c41af4469d4fcf731440ecd17821ae7a",
+    "url": "/cwg-responsive-tester/static/media/loader.c41af446.svg"
+  }
+]);
